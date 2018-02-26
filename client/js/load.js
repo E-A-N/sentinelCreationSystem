7d149e8fd5fb0ea5abcce579df3a5716d8a1389e
e-a-n <ean.director@gmail.com>
2018-02-25 17:22:51 -0800

    fixed bug in menu (loading false referenced plugins) and cleaned up code

--- client/js/load.js
+++ client/js/load.js
@@ -2,11 +2,6 @@ var loadState = {
 
     preload: function () {
 
-        /*
-        Load all game assets
-        Place your load bar, some messages.
-        In this case of loading, only text is placed...
-        */
 
         var loadingLabel = game.add.text(80, 150, 'loading...', {font: '30px Courier', fill: '#fff'});
 
@@ -19,7 +14,6 @@ var loadState = {
 
         //Load your data, JSON, Querys...
         //Example: game.load.json('version', 'http://phaser.io/version.json');
-
     },
 
     create: function () {
--- client/js/uiTest.js
+++ client/js/uiTest.js
@@ -1,16 +1,11 @@
-var uiTest. = {}
+var uiTest = {};
 uiTest.preload = function(){
+    uiTest.btnKey = "btn1";
     game.stage.backgroundColor = '#85b5e1';
-    game.load.baseURL = 'http://examples.phaser.io/assets/';
-    game.load.crossOrigin = 'anonymous';
-    game.load.image('player', 'sprites/phaser-dude.png');
-    game.load.image('platform', 'sprites/platform.png');
-}
-
-
+    game.load.atlasJSONHash(uiTest.btnKey, "assets/sprites/ui/spriteSheet.png", "assets/sprites/ui/references.json", "reen_boxTick.png");
+};
 
 uiTest.create = function() {
-    uiTest.player = game.add.sprite(100, 200, 'player');
 
     var clickEv = function(spr){
         if (spr.name === "left"){
@@ -22,48 +17,20 @@ uiTest.create = function() {
     };
     var onOver = function(spr){
         spr.tint = 0xFFFFFF;
-    }
+    };
     var onOut = function(spr){
         spr.tint = 0;
-    }
+    };
 
-    uiTest.leftBtn = game.add.button(50, 200, 'player', clickEv, this);
-    uiTest.rightBtn = game.add.button(150, 200, 'player', clickEv, this);
+    uiTest.leftBtn = game.add.button(50, 200, uiTest.btnKey, clickEv, this);
+
 
     leftBtn = uiTest.leftBtn;
-    rightBtn = uiTest.rightBtn;
     leftBtn.name = "left";
-    rightBtn.name = "right";
-
     leftBtn.inputEnabled = true;
-    rightBtn.inputEnabled = true;
-
     leftBtn.onInputOver.add(onOver, this);
-    rightBtn.onInputOver.add(onOver, this);
     leftBtn.onInputOut.add(onOut, this);
-    rightBtn.onInputOut.add(onOut, this);
     leftBtn.tint = 0;
-    rightBtn.tint = 0;
+};
 
-
-    game.physics.arcade.enable(player);
-
-    player.body.collideWorldBounds = true;
-    player.body.gravity.y = 500;
-
-    uiTestplatforms = game.add.physicsGroup();
-
-    platforms.create(500, 150, 'platform');
-    platforms.create(-200, 300, 'platform');
-    platforms.create(400, 450, 'platform');
-
-    platforms.setAll('body.immovable', true);
-
-    cursors = game.input.keyboard.createCursorKeys();
-    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
-}
-
-function update () {
-
-}
+uiTest.update = function() {};
